a2b724d0a9d13d32089bb9b2a08769a569eeccf3
pinkun palei <pinkunpalei6@gmail.com>
2024-01-30 17:44:27 +0530

    Add new file

---
+++ Midium/maximumSubarray.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int n=nums.length;
+        int curSum=0;
+        int maxSum=Integer.MIN_VALUE;
+        for(int i=0;i<n;i++){
+            curSum+=nums[i];
+            maxSum=Math.max(curSum,maxSum);
+            if(curSum < 0){
+               curSum=0;
+            }
+            
+        }
+        return maxSum;
+    }
+}
